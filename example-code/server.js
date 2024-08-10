770949415c3fbe396cf43be63231008aaab2c58d
User9644 <141528905+User9644@users.noreply.github.com>
2024-08-10 19:25:47 +0200

    Create server.js

---
+++ example-code/server.js
@@ -0,0 +1,16 @@
+const EasyWSS = require('easier-ws');
+const { printc } = require('better-node-print');
+
+const server = new EasyWSS({port: 8080});
+
+server.on("message", (socket, msg) => {
+    printc([
+        {text: socket.remoteAddress, color: "yellow"},
+        {text: " sendet Nachricht: "},
+        {text: JSON.stringify(msg), color: "blue"}
+    ]);
+    server.sendAll(msg);
+
+    if(msg.value == "!disconnect all")
+        server.disconnectAll();
+});
